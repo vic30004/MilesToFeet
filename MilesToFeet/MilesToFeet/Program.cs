37bbe7b027f81887cfd6b9e99495e6933d718200
vic30004 <victor_993@msn.com>
2020-01-22 21:43:02 +0200

    Completed the mini project

--- MilesToFeet/MilesToFeet/Program.cs
+++ MilesToFeet/MilesToFeet/Program.cs
@@ -12,8 +12,6 @@ namespace MilesToFeet
         
         static void Main(string[] args)
         {
-            int mileToFeet = 5280;
-            int mileToInches = 63360;
             bool end = false;
             int convertedValue = 0;
 
@@ -24,7 +22,7 @@ namespace MilesToFeet
 
             do
             {
-                if (userInput.ToLower() == "f")
+                if (userInput.ToLower() == "f" || userInput.ToLower() == "feet")
                 {
                     Console.WriteLine("Great! we will convert miles to feet! Please input the number you would like to convert.");
                     string userValue = Console.ReadLine();
@@ -45,6 +43,41 @@ namespace MilesToFeet
                         end = false;
                     }
 
+
+                }
+                else if (userInput.ToLower() == "i" || userInput.ToLower()=="inches")
+                {
+                    Console.WriteLine("Great! we will convert miles to inches! Please input the number you would like to convert.");
+                    string userValue = Console.ReadLine();
+                    int number;
+                    bool value = int.TryParse(userValue, out number);
+                    if (value)
+                    {
+                        convertedValue = convertToFeet(number);
+                        Console.WriteLine("{0} miles is {1} inches", number, convertedValue);
+                        end = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please make sure your adding a number!");
+                        askQuestion();
+                        userInput = Console.ReadLine();
+
+                        end = false;
+                    }
+                }
+                else if(userInput.ToLower()=="q" || userInput.ToLower() == "quit")
+                {
+                    Console.WriteLine("Sorry to see you go. We hope to see you soon!");
+                    end = true;
+                }
+                else
+                {
+                    Console.WriteLine("Please Make sure you write either i for inches or f for feet");
+                    askQuestion();
+                    userInput = Console.ReadLine();
+
+                    end = false;
                 }
             }
 
@@ -57,7 +90,7 @@ namespace MilesToFeet
 
         public static  void askQuestion()
         {
-            Console.WriteLine("Would you like to convert miles to inches or feet? Please use i for inches and f for feet");
+            Console.WriteLine("Would you like to convert miles to inches or feet? Please use i for inches and f for feet or write q to quit.");
         
         }
 
@@ -67,6 +100,11 @@ namespace MilesToFeet
             return convertedValue;
         }
 
+        public static int convertToInches(int num)
+        {
+            int convertedValue = num * 63360;
+            return convertedValue;
+        }
        
     }
 }

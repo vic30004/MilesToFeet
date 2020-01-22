989643035ff64a6f9b11161bffcd3421041c82f6
vic30004 <victor_993@msn.com>
2020-01-22 20:25:29 +0200

    Added variablels

--- MilesToFeet/MilesToFeet/Program.cs
+++ MilesToFeet/MilesToFeet/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 
+/*
+ 1 mile = 5280 feet
+ 1 mile =   63360 inches
+
+    */
 namespace MilesToFeet
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int mileToFeet = 5280;
+            int mileToInches = 63360;
+
+            Console.WriteLine("Would you like to convert miles to inches or feet? Please use i for inches and f for feet");
+
+            string userInput = Console.ReadLine();
+            
+            Console.WriteLine(userInput);
+
         }
+
+
     }
 }

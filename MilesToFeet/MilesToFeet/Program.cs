eac042af9cacdbaa234dd851395d3685f9c8c385
vic30004 <victor_993@msn.com>
2020-01-22 21:08:43 +0200

    Added func to convert miles to feet

--- MilesToFeet/MilesToFeet/Program.cs
+++ MilesToFeet/MilesToFeet/Program.cs
@@ -14,14 +14,38 @@ namespace MilesToFeet
             int mileToFeet = 5280;
             int mileToInches = 63360;
 
+            int convertedValue = 0;
+
             Console.WriteLine("Would you like to convert miles to inches or feet? Please use i for inches and f for feet");
 
             string userInput = Console.ReadLine();
             
-            Console.WriteLine(userInput);
+            if (userInput.ToLower() == "f")
+            {
+                Console.WriteLine("Great! we will convert miles to feet! Please input the number you would like to convert.");
+                string userValue = Console.ReadLine();
+                int number;
+                bool value = int.TryParse(userValue, out number);
+                if (value)
+                {
+                    convertToFeet(number);
+                    Console.WriteLine("{0} miles is {1} feet", number,convertedValue );
+                }
+                else
+                {
+                    Console.WriteLine("Please make sure your adding a number!");
+                }
+
+            }
 
         }
+        
+        public static int convertToFeet(int num)
+        {
+            int convertedValue = num * 5280;
+            return convertedValue;
+        }
 
-
+       
     }
 }
